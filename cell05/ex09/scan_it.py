9c0598f6bb6190fefe972633528dcf1f821790f0
Penpitcha <an.penpitcha@gmail.com>
2026-02-13 02:17:26 +0700

    add ex09 and scan_it.py

---
+++ cell05/ex09/scan_it.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import sys
+
+param=len(sys.argv)-1
+if(param!=2):
+	print("none")
+else:
+	count=0
+	arr=sys.argv[2].split(" ")
+	for i in arr:
+		if(i==sys.argv[1]):
+			count+=1
+	print(count)
